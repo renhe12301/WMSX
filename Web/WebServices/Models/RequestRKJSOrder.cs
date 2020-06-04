ad68f972bf43f5e9c819b34a7cc8693d70a06b51
renhe <59413357+renhe@users.noreply.github.com>
2020-06-04 12:43:29 +0800

    修正集约化物资系统对接接口的字段别名

--- Web/WebServices/Models/RequestRKJSOrder.cs
+++ Web/WebServices/Models/RequestRKJSOrder.cs
@@ -2,52 +2,86 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 namespace Web.WebServices.Models
 {
-    [Serializable]
     [DataContract]
     public class RequestRKJSOrder
     {
+        [JsonProperty(PropertyName ="headerId")]
         [DataMember]
         public string HeaderId { get; set; }
+        
+        [JsonProperty(PropertyName ="documentNumber")]
         [DataMember]
         public string DocumentNumber { get; set; }
+        
+        [JsonProperty(PropertyName ="documentType")]
         [DataMember]
         public string DocumentType { get; set; }
+        
+        [JsonProperty(PropertyName ="managerId")]
         [DataMember]
         public string ManagerId { get; set; }
+        
+        [JsonProperty(PropertyName ="ouCode")]
         [DataMember]
         public string OuCode { get; set; }
+        
+        [JsonProperty(PropertyName ="organizationCode")]
         [DataMember]
         public string OrganizationCode { get; set; }
+        
+        [JsonProperty(PropertyName ="vendorId")]
         [DataMember]
         public string VendorId { get; set; }
+        
+        [JsonProperty(PropertyName ="vendorSiteId")]
         [DataMember]
         public string VendorSiteId { get; set; }
+        
+        [JsonProperty(PropertyName ="businessType")]
         [DataMember]
         public string BusinessType { get; set; }
+        
+        [JsonProperty(PropertyName ="currency")]
         [DataMember]
         public string Currency { get; set; }
+        
+        [JsonProperty(PropertyName ="totalAmount")]
         [DataMember]
         public string TotalAmount { get; set; }
+        
+        [JsonProperty(PropertyName ="exitEntryDate")]
         [DataMember]
         public string ExitEntryDate { get; set; }
+        
+        [JsonProperty(PropertyName ="creationDate")]
         [DataMember]
         public string CreationDate { get; set; }
+        
+        [JsonProperty(PropertyName ="erpItemId")]
         [DataMember]
         public string ItemId { get; set; }
+        
+        [JsonProperty(PropertyName ="remark")]
         [DataMember]
         public string Remark { get; set; }
+        
+        [JsonProperty(PropertyName ="addFlag")]
         [DataMember]
         public string AddFlag { get; set; }
         
+        [JsonProperty(PropertyName ="result")]
         [DataMember]
         public string Result { get; set; }
 
+        [JsonProperty(PropertyName ="errMsg")]
         [DataMember]
         public string ErrMsg { get; set; }
 
+        [JsonProperty(PropertyName ="receiptLines")]
         [DataMember]
         public List<RequestRKJSRow> RequestRKJSRows = new List<RequestRKJSRow>();
 
--- Web/WebServices/Models/RequestRKJSRow.cs
+++ Web/WebServices/Models/RequestRKJSRow.cs
@@ -1,39 +1,66 @@
 using System;
 using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 namespace Web.WebServices.Models
 {
-    [Serializable]
     [DataContract]
     public class RequestRKJSRow
     {
+        
+        [JsonProperty(PropertyName ="lineId")]
         [DataMember]
         public string LineId { get; set; }
+        
+        [JsonProperty(PropertyName ="headerId")]
         [DataMember]
         public string HeaderId { get; set; }
+        
+        [JsonProperty(PropertyName ="lineNumber")]
         [DataMember]
         public string LineNumber { get; set; }
+        
+        [JsonProperty(PropertyName ="materialId")]
         [DataMember]
         public string MaterialId { get; set; }
+        
+        [JsonProperty(PropertyName ="processingQuantity")]
         [DataMember]
         public string ProcessingQuantity { get; set; }
+        
+        [JsonProperty(PropertyName ="price")]
         [DataMember]
         public string Price { get; set; }
+        
+        [JsonProperty(PropertyName ="amount")]
         [DataMember]
         public string Amount { get; set; }
+        
+        [JsonProperty(PropertyName ="taskId")]
         [DataMember]
         public string TaskId { get; set; }
+        
+        [JsonProperty(PropertyName ="expenditureType")]
         [DataMember]
         public string ExpenditrueType { get; set; }
+        
+        [JsonProperty(PropertyName ="remark")]
         [DataMember]
         public string Remark { get; set; }
+        
+        [JsonProperty(PropertyName ="addFlag")]
         [DataMember]
         public string AddFlag { get; set; }
+        
+        [JsonProperty(PropertyName ="relatedId")]
         [DataMember]
         public string RelatedId { get; set; }
+        
+        [JsonProperty(PropertyName ="result")]
         [DataMember]
         public string Result { get; set; }
 
+        [JsonProperty(PropertyName ="errMsg")]
         [DataMember]
         public string ErrMsg { get; set; }
         
